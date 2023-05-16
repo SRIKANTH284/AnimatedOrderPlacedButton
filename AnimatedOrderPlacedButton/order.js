84d8efa399a4dee388b46865ac8500e3730293a8
BADAVATH SRIKANTH <80474350+SRIKANTH284@users.noreply.github.com>
2023-05-16 23:17:27 +0530

    Add files via upload

---
+++ AnimatedOrderPlacedButton/order.js
@@ -0,0 +1,9 @@
+$('.order').click(function (e) {
+	let button = $(this); 
+	if (!button.hasClass("animate")) {
+		button.addClass('animate');
+		setTimeout(() => {
+			button.removeClass('animate');
+		}, 10000)		  
+	}
+});
